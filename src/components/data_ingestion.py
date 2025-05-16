9aefccd5b05df529fd4e593a519626f8407640e1
saidabroor <saidabrorkhon199919@gmail.com>
2025-05-16 16:45:43 +0900

    logging and exception

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,14 @@
+# data_ingestion.py -  Datani yuklash uchun ishlatiladigan file.
+# data_transformation.py - datani tayyorlash, preprocessing va datani qayta ishlash uchun ochiladigan file.
+
+# model_trainer.py - model training uchun ishlatiladigan file.
+
+# Exception handling - dastur ishlashda xatolik yuzaga kelganda, tizimga xabar berish, xatolikni hal qilish va dastur ishini uzluksiz davom ettirish uchun kerakli choralarni ko'rish jarayoni. Maqsad kodni barqaror va ishonchli ishlashiga yordam berish.
+
+# Logging - tizimda yuz berayotgan jarayonlarni, katta-kichik o'zgarishlar, xatoliklar, muhim voqealarni qayd etib borish. Real-timeda oson tracking qilish imkoni va tahlil qilish imkoni bor.
+
+# utils.py - filening ichida functionlar bo'ladi va bog'lovchi vazifasini o'taydi.
+
+
+
+
