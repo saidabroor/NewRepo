59c7a33b37582686938cab7718c6151485621ca6
saidabroor <saidabrorkhon199919@gmail.com>
2025-05-20 14:40:12 +0900

    data ingestion

--- src/logging.py
+++ src/logging.py
@@ -2,14 +2,15 @@ import logging
 from datetime import datetime
 import os
 
-LOG_FILE = f"{datetime.now().strftime('%m_%d,%_Y,%_H,%_M,_%_S')}.log"
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
-log_path = os.path.join(os.getcwd(),'logs',LOG_FILE)
+log_dir = os.path.join(os.getcwd(), 'logs')
+os.makedirs(log_dir, exist_ok=True)
 
-os.makedirs(log_path, exist_ok=True)
+LOG_FILE_PATH = os.path.join(log_dir, LOG_FILE)
 
 logging.basicConfig(
-  filename=LOG_FILE,
-  format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-  level=logging.INFO
+    filename=LOG_FILE_PATH,
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO
 )
